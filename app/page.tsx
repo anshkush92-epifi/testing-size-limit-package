ead828f8047ad181d1e2e7efdc2ab5c1184b9e07
anshkush92-epifi <ansh.singh@epifi.com>
2025-11-05 13:52:06 +0530

    Update size-limit configuration in package.json to increase static assets limit and set dynamic rendering to force-static in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import ComboBox from "./ComboBox";
 
+export const dynamic = 'force-static';
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
